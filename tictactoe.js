fb75c1cbfa1edf1205971bfef3a3c785c44534bc
Nicholas Lam <nicholas.lfh91@gmail.com>
2024-12-18 01:45:27 +0800

    tictactoe added eventListener for X and O

---
+++ tictactoe.js
@@ -0,0 +1,84 @@
+// alert("Hello, hope you're ready to play TIC-TAC-TOE!")
+// alert("Player 1 will start first followed by Player 2!")
+// alert("First to line up 3 boxes wins, good luck!")
+
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+// const selection = ["p1Click","p2Click"]
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+// let = player1;
+// let = player2;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+// when button triggers, chosen square box will "X" or "O" depending on player.
+
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+// let count = 10;
+// for (let i= 0; i < count.length; i++) {
+//     if ()
+//     // document.getElementById("h1").textContent = "Player 2's turn!"
+// }
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+// changeText.addEventListener("click", function() {
+//     changeText.textContent = "Text has been changed!";
+//   });
+
+
+
+const firstSqr = addEventListener("click", function() {
+    
+    document.getElementById("0").textContent = "X";
+    document.getElementById("0").textContent = "O";
+    
+});
+
+const secondaSqr = addEventListener("click", function() {
+    document.getElementById("1").textContent = "X";
+});
+
+const thirdSqr = addEventListener("click", function() {
+    document.getElementById("2").textContent = "X";
+});
+
+const fourthSqr = addEventListener("click", function() {
+    document.getElementById("3").textContent = "X";
+});
+
+const fifthqr = addEventListener("click", function() {
+    document.getElementById("4").textContent = "X";
+});
+
+const sixthSqr = addEventListener("click", function() {
+    document.getElementById("5").textContent = "X";
+});
+
+const seventhSqr = addEventListener("click", function() {
+    document.getElementById("6").textContent = "X";
+});
+
+const eightSqr = addEventListener("click", function() {
+    document.getElementById("7").textContent = "X";
+});
+const nineSqr = addEventListener("click", function() {
+    document.getElementById("8").textContent = "X";
+});
+
+
+
+
+
+// const changeText = addEventListener("click", function() {
+//     document.getElementById("test").textContent = "wow it works";
+// });
