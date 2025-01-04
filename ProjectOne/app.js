17b63524c5581de7c420f7bd5c5c4bac9641bd01
Nicholas Lam <nicholas.lfh91@gmail.com>
2025-01-04 20:57:56 +0800

    troubleshooting board

--- ProjectOne/app.js
+++ ProjectOne/app.js
@@ -1,33 +1,29 @@
-const tilesCon = document.querySelector("mainBoard");   
+const tilesContainer = document.querySelector(".tiles"); 
 const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
-const colorsPicklist = [...colors, ...colors];
-const tileCount = colorsPicklist.length;
+const colorPicklist = [...colors,...colors];
+tileCount = colorPicklist.length
 
-// Game state
+// game state 
 let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
 function buildTile(color){
     const element = document.createElement("div");
+
     element.classList.add("tile");
     element.setAttribute("data-color", color);
 
     return element;
-
 }
 
-// build up tile randomly
-
-for (let i=0; i< tileCount; i++){
-    const randomTileGeneration = Math.floor(Math.random() * colorsPicklist.length)
-    const colorsPicklist = colorsPicklist[randomTileGeneration];
+// build up tiles 
+for (i=0; i<tileCount; i++) {
+    const randomColor = Math.floor(Math.random() * colorPicklist.length);
+    const color = colorPicklist[randomColor]
     const tile = buildTile(color);
-    
+    colorPicklist.splice(randomColor, 1);
+    tilesContainer.appendChild(tile);
 
-// remove duplicates
-
-    colorsPicklist.splice(randomTileGeneration, 1);
-    document.body.appendChild(tile);
+    console.log(tile);
 }
-
