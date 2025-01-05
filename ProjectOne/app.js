13e333b90e74e8172b70a79ffd4c44cc20eba097
Nicholas Lam <nicholas.lfh91@gmail.com>
2025-01-06 00:39:13 +0800

    board done

--- ProjectOne/app.js
+++ ProjectOne/app.js
@@ -1,29 +1,69 @@
-const tilesContainer = document.querySelector(".tiles"); 
+const tilesContainer = document.querySelector(".tiles");
 const colors = ["aqua", "aquamarine", "crimson", "blue", "dodgerblue", "gold", "greenyellow", "teal"];
-const colorPicklist = [...colors,...colors];
-tileCount = colorPicklist.length
+const colorPickList = [...colors, ...colors];
+const tileCount = colorPickList.length;
 
-// game state 
+//game state
 let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfMove = false;
 
 function buildTile(color){
     const element = document.createElement("div");
-
+    
     element.classList.add("tile");
     element.setAttribute("data-color", color);
+    
+
+    element.addEventListener("click", () => {
+        if (awaitingEndOfMove) {
+            return;
+        }
+
+
+        element.style.backgroundColor = color;
+
+        if (!activeTile) {
+            activeTile = element;
+
+            return;
+        }
+       
+        const colorToMatch = activeTile.getAttribute("data-color");
+
+        if(colorToMatch === color){
+            awaitingEndOfMove = false;
+            activeTile = null;
+            revealedCount += 2;
+
+            if (revealedCount === tileCount){
+                alert("you win!");
+
+            }
+            return;
+        }
+
+        awaitingEndOfMove = true;
+        setTimeout(() => {
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            awaitingEndOfMove = false;
+            activeTile = null;
+        }, 1000);
+    });
 
     return element;
 }
 
-// build up tiles 
-for (i=0; i<tileCount; i++) {
-    const randomColor = Math.floor(Math.random() * colorPicklist.length);
-    const color = colorPicklist[randomColor]
+// build up tile 
+for (let i=0; i < tileCount; i++){
+    // generate random number to assign tile
+    const randomIndex = Math.floor(Math.random() * colorPickList.length);
+    const color = colorPickList[randomIndex];
     const tile = buildTile(color);
-    colorPicklist.splice(randomColor, 1);
+    
+    // clear dups
+    colorPickList.splice(randomIndex, 1);
     tilesContainer.appendChild(tile);
-
-    console.log(tile);
 }
